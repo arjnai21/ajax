e544d8a138b1fb4de43903b00bc5fbbac65c173d
arjun <arjun@nairsnet.com>
2021-08-01 23:35:56 -0400

    cleanup

--- lib/models/payment.dart
+++ lib/models/payment.dart
@@ -9,33 +9,14 @@ class Payment{
   Payment(this.sender, this.recipient, this.amount, this.message,
       this.timestamp);
 
-  Payment.fromSnapshot(snapshot){
-    sender = snapshot["senderUid"];
-    recipient = snapshot["recipientUid"];
-    amount = snapshot["amount"];
-    message = snapshot["message"];
-    timestamp = snapshot["timestamp"].toDate();
-
-  }
-
   Payment.fromJson(Map<String, dynamic> json){
-    print("getting payments");
     sender = json["sender_id"];
     recipient = json["recipient_id"];
     amount = json["amount"];
     message = json["message"];
     timestamp = DateTime.parse(json["timestamp"]).toLocal();
-    print(timestamp);
 
   }
 
-  // static getDummyTransactions(){
-  //   return [
-  //     Payment("Arjun", "Wilbert", 200, "hello", DateTime.utc(1969, 7, 20, 20, 18, 04)),
-  //     Payment("Arjun", "Wilbert", 200, "hello", DateTime.utc(1969, 7, 20, 20, 18, 04)),
-  //     Payment("Arjun", "Wilbert", 200, "hello", DateTime.utc(1969, 7, 20, 20, 18, 04)),
-  //
-  //   ];
-  // }
 }
 
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -16,8 +16,4 @@ class AjaxUser {
     pfpUrl = json["photo_url"];
   }
 
-  static getDummyAccount() {
-    return AjaxUser("123456", "anair123", "arjun@nairsnet.com", 21.87,
-        "https://dw3jhbqsbya58.cloudfront.net/rosters/a9cbf684-16ef-4997-9539-15fefe9df410/e/5/5/e55dc629-91aa-ea11-80ce-a444a33a3a97/thumbnail.jpg?version=637389721377004039");
-  }
 }
