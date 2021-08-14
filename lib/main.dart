e4efa4df0b4037b4ca4f61f460d6c24cac592fd4
Arjun Nair <arjun@nairsnet.com>
2021-08-14 18:31:38 -0400

    tseting out github codespaces

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-
+// testing out github codespaces
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
